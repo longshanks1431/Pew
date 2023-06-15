b3bc61024755d46d32a27eb9bd70ea4cd9716e21
longshanks1431 <davidcarlson11@gmail.com>
2023-06-14 21:10:15 -0500

    created stable game loop structure w/ 120FPS

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,9 +1,11 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
 
     public Game() {
         //this constructor is the head method of the class - whenever we create object, we call this
@@ -13,7 +15,41 @@ public class Game {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
 
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        while(true) {
+
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+
+        }
     }
     
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -8,28 +10,53 @@ import inputs.KeyboardInputs;
 
 public class GamePanel extends JPanel {
 
-    private int xDelta, yDelta = 0;
+    private float xDelta, yDelta = 100;
+    private float xDir = 2f, yDir = 2f;
+    private Color color = new Color(150, 20, 90);
+    private Random random;
 
     public GamePanel() {
-        
+        random = new Random();
         addKeyListener(new KeyboardInputs(this));
     }
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
-        g.fillRect(50 + xDelta, 300 + yDelta, 50, 70);
 
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int)xDelta, (int)yDelta, 50, 70);
+
+    }
+
+    private void updateRectangle() {
+        xDelta+=xDir;
+        if (xDelta > 1280 || xDelta < 0) {
+            xDir *= -1;
+            color = getRndColor();
+
+        }    
+        yDelta+=yDir;
+        if (yDelta > 1280 || yDelta < 0) {
+            yDir *= -1;
+            color = getRndColor();
+        }
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return new Color(r,g,b);
     }
     
 }
